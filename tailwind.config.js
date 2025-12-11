78d9abfa7af050f65ff638b1f7d541deeb5d66a1
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-11 22:51:13 +0600

    added postcss.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -5,5 +6,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-};
+  plugins: [],
+}
