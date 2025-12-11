e03d34dff2d2412650b21d18f0c2941f74460021
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-11 20:43:20 +0600

    used baseNode component to the textNonde componnent

--- src/nodes/textNode.jsx
+++ src/nodes/textNode.jsx
@@ -1,35 +1,33 @@
-// textNode.js
+import React, { useState } from "react";
+import BaseNode from "./BaseNode";
 
-import { useState } from 'react';
-import { Handle, Position } from 'reactflow';
-
-export const TextNode = ({ id, data }) => {
-  const [currText, setCurrText] = useState(data?.text || '{{input}}');
+const TextNode = ({ id, data }) => {
+  const [currText, setCurrText] = useState(data?.text || "{{input}}");
 
   const handleTextChange = (e) => {
     setCurrText(e.target.value);
+
+    // keep value in ReactFlow node data
+    if (data?.setText) {
+      data.setText(e.target.value);
+    }
   };
 
   return (
-    <div style={{width: 200, height: 80, border: '1px solid black'}}>
-      <div>
-        <span>Text</span>
-      </div>
-      <div>
-        <label>
-          Text:
-          <input 
-            type="text" 
-            value={currText} 
-            onChange={handleTextChange} 
-          />
-        </label>
-      </div>
-      <Handle
-        type="source"
-        position={Position.Right}
-        id={`${id}-output`}
+    <BaseNode
+      title="Text"
+      inputs={data.inputs || []}
+      outputs={data.outputs || [`${id}-output`]}
+    >
+      <input
+        type="text"
+        value={currText}
+        onChange={handleTextChange}
+        className="input input-bordered w-full"
+        placeholder="Enter text..."
       />
-    </div>
+    </BaseNode>
   );
-}
+};
+
+export default TextNode;
